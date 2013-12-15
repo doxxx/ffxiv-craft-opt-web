2584a8a179ecd2eb8ec7589f7052265860430ccb
Gordon Tyler <gordon@doxxx.net>
2013-12-14 20:58:17 -0500

    Streamline app for deployment.
    - Switched to min versions of angularjs and bootstrap css and js.
    - Removed unused angularjs features (routes and filters).

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,9 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 var app = angular.module('ffxivCraftOptWeb', [
   'ui.bootstrap',
-  'ngRoute',
-  'ffxivCraftOptWeb.filters',
   'ffxivCraftOptWeb.services',
   'ffxivCraftOptWeb.directives',
-  'ffxivCraftOptWeb.controllers'
+  'ffxivCraftOptWeb.controllers',
 ]);
-
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
