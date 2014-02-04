2721a03f9df197d414bd5affd88d5ef4c08c590d
Gordon Tyler <gordon@doxxx.net>
2014-02-04 09:23:59 -0500

    Fix initialization of Logger with null logOutput.

--- app/js/ffxivcraftmodel.js
+++ app/js/ffxivcraftmodel.js
@@ -5,7 +5,7 @@ function Logger(logOutput) {
     this.log = function(myString) {
         var args = Array.prototype.slice.call(arguments, 1);
         var msg = String.prototype.sprintf.apply(myString, args);
-        if (this.logOutput !== undefined) {
+        if (this.logOutput !== undefined && this.logOutput !== null) {
           this.logOutput.write(msg + '\n');
         }
         else {
