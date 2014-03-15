249f03836d9bfa287dd8f3a92494a4341798a476
Gordon Tyler <gordon@doxxx.net>
2014-03-15 12:33:24 -0400

    Enter key in login modal should act like clicking the login button.

--- app/js/directives.js
+++ app/js/directives.js
@@ -154,3 +154,16 @@ module.directive('autoFocus', function($timeout) {
     }
   };
 });
+
+module.directive('onEnter', function () {
+  // Linker function
+  return function (scope, element, attrs) {
+    element.bind('keypress', function (event) {
+      if (event.keyCode == 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs['onEnter']);
+        });
+      }
+    });
+  };
+});
