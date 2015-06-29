17132fd3554bf151920266836830988bd3a7d679
Gordon Tyler <gordon@doxxx.net>
2015-06-29 10:59:56 -0400

    Launch dev web server with 'app' folder as root.

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -6,6 +6,7 @@ var DEFAULT_PORT = 8001;
 
 function main(argv) {
   var port = Number(argv[2]) || DEFAULT_PORT;
+  assetCache.setCwd(argv[3]);
   assetCache.listen(port, function() {
     console.log('\tAsset server listening on port', port);
   });
