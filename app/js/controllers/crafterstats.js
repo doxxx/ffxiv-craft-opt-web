40f6ba9eee5298a148b5667eb9626b758cdf55ce
Gordon Tyler <gordon@doxxx.net>
2015-07-19 16:02:39 -0400

    Use tabs instead of class selector bar in Crafter Attributes page.

--- app/js/controllers/crafterstats.js
+++ app/js/controllers/crafterstats.js
@@ -1,6 +1,12 @@
 "use strict";
 
-angular.module('ffxivCraftOptWeb.controllers').controller('CrafterStatsController', function ($scope) {
+angular.module('ffxivCraftOptWeb.controllers').controller('CrafterStatsController', function ($scope, _allClasses) {
+  // Initialize tab names and initial active state
+  $scope.tabs = [];
+  for (var i = 0; i < _allClasses.length; i++) {
+    var cls = _allClasses[i];
+    $scope.tabs.push({ name: cls, active: cls === $scope.recipe.cls });
+  }
 
   $scope.toggleAction = function (action) {
     var i = $scope.crafter.stats[$scope.crafter.cls].actions.indexOf(action);
