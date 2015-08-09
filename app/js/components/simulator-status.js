6302f79351a539bdf8c630d0be9c936de512687c
Gordon Tyler <gordon@doxxx.net>
2015-08-09 19:46:57 -0400

    Fix simulator status updates. For realsies this time.

--- app/js/components/simulator-status.js
+++ app/js/components/simulator-status.js
@@ -12,13 +12,13 @@ angular.module('ffxivCraftOptWeb.components')
         valid: '&'
       },
       controller: function ($scope) {
-        var update = function (event, newState) {
-          if (newState) {
-            $scope.durability = newState.durability;
-            $scope.condition = newState.condition;
-            $scope.progress = newState.progress;
-            $scope.quality = newState.quality;
-            $scope.cp = newState.cp;
+        var update = function () {
+          if ($scope.status.state) {
+            $scope.durability = $scope.status.state.durability;
+            $scope.condition = $scope.status.state.condition;
+            $scope.progress = $scope.status.state.progress;
+            $scope.quality = $scope.status.state.quality;
+            $scope.cp = $scope.status.state.cp;
           }
           else {
             $scope.durability = $scope.recipe.durability;
@@ -32,14 +32,13 @@ angular.module('ffxivCraftOptWeb.components')
           $scope.qualityPercent = Math.min(100, $scope.quality / $scope.recipe.maxQuality * 100);
           $scope.cpPercent = Math.min(100, $scope.cp / $scope.crafter.cp * 100);
 
-          $scope.hqPercent = newState && newState.hqPercent || 0;
-          $scope.successPercent = newState && newState.successPercent || 0;
+          $scope.hqPercent = $scope.status.state && $scope.status.state.hqPercent || 0;
+          $scope.successPercent = $scope.status.state && $scope.status.state.successPercent || 0;
         };
 
         $scope.$watchCollection("crafter", update);
         $scope.$watchCollection("recipe", update);
         $scope.$watchCollection("status", update);
-        $scope.$watchCollection("status.state", update);
 
         update();
       }
