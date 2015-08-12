f746101139227b69bd93acd8abbdfadd8809cbf9
Rhoda Baker <rhoda.baker@gmail.com>
2015-08-11 21:47:07 -0400

    (Re-)Implement preciseTouch.

--- app/js/services/actions.js
+++ app/js/services/actions.js
@@ -42,7 +42,7 @@ var allActions = [
 
   // Heavensward
   //{"shortName": "byregotsBrow", "name": "Byregot's Brow", "cls": "All", "level": 51, "common": true, "skillID": {"Alchemist": 100126, "Armorer": 100122, "Blacksmith": 100121, "Carpenter": 100120, "Culinarian": 100127, "Goldsmith": 100123, "Leatherworker": 100124, "Weaver": 100125}},
-  //{"shortName": "preciseTouch", "name": "Precise Touch", "cls": "All", "level": 53, "skillID": {"Alchemist": 100134, "Armorer": 100130, "Blacksmith": 100129, "Carpenter": 100128, "Culinarian": 100135, "Goldsmith": 100131, "Leatherworker": 100132, "Weaver": 100133}},
+  {"shortName": "preciseTouch", "name": "Precise Touch", "cls": "All", "level": 53, "skillID": {"Alchemist": 100134, "Armorer": 100130, "Blacksmith": 100129, "Carpenter": 100128, "Culinarian": 100135, "Goldsmith": 100131, "Leatherworker": 100132, "Weaver": 100133}},
   //{"shortName": "makersMark", "name": "Maker's Mark", "cls": "Goldsmith", "level": 54, "skillID": {"Goldsmith": 100178}},
   //{"shortName": "muscleMemory", "name": "Muscle Memory", "cls": "Culinarian", "level": 54, "skillID": {"Culinarian": 100136}},
   //{"shortName": "satisfaction", "name": "Satisfaction", "cls": "All", "level": 55, "common": true, "skillID": {"Alchemist": 100175, "Armorer": 100171, "Blacksmith": 100170, "Carpenter": 100169, "Culinarian": 100176, "Goldsmith": 100172, "Leatherworker": 100173, "Weaver": 100174}},
@@ -138,7 +138,7 @@ var actionGroups = [
     "hastyTouch",
     "byregotsBlessing",
     //"byregotsBrow",
-    //"preciseTouch",
+    "preciseTouch",
     //"innovativeTouch",
     //"byregotsMiracle",
     //"trainedHand"
