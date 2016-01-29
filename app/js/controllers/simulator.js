e6c16f6532a2110891eb99239146af61aba9fc0b
Nicolas MARQUES <nmarques-ext@delfingen.com>
2016-01-29 10:48:44 +0100

    Improve recipe list filtering :
    * Restrict recipe list to crafter level : a crafter level 26 can only
    craft level 31 (26 + 5) recipe max.
    
    * Ignore case and accent : searching on "poele a frire", will return
    "Poêle à frire" (in french language for exemple)

--- app/js/controllers/simulator.js
+++ app/js/controllers/simulator.js
@@ -1,5 +1,29 @@
 "use strict";
 
+// Remove Accent from source (ex: ùéèûà will be returned as ueeua).
+// Use to compare string without accents
+// ---------------------------------------------------
+String.prototype.removeAccent = function(){
+    var accent = [
+            /[\300-\306]/g, /[\340-\346]/g, // A, a
+            /[\310-\313]/g, /[\350-\353]/g, // E, e
+            /[\314-\317]/g, /[\354-\357]/g, // I, i
+            /[\322-\330]/g, /[\362-\370]/g, // O, o
+            /[\331-\334]/g, /[\371-\374]/g, // U, u
+            /[\321]/g, /[\361]/g, // N, n
+            /[\307]/g, /[\347]/g, // C, c
+        ],
+        noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+
+    var str = this;
+    for (var i = 0; i < accent.length; i++){
+        str = str.replace(accent[i], noaccent[i]);
+    }
+
+    return str;
+};
+
+
 angular.module('ffxivCraftOptWeb.controllers').controller('SimulatorController', function ($scope, $filter, $modal,
   $rootScope, $translate, $timeout, $state, _recipeLibrary, _simulator, _actionsByName)
 {
@@ -39,7 +63,20 @@ angular.module('ffxivCraftOptWeb.controllers').controller('SimulatorController',
     $scope.recipeSearch.loading = true;
     var p = _recipeLibrary.recipesForClass($translate.use(), $scope.recipe.cls);
     p.then(function (recipes) {
-      $scope.recipeSearch.list = $filter('filter')(recipes, {name: $scope.recipeSearch.text});
+        // Restrict recipes to crafter level
+        recipes  = $filter('filter')(recipes, {baseLevel: $scope.crafter.stats[$scope.recipe.cls].level}, function(recipeLevel, crafterLevel){
+            if (!crafterLevel || crafterLevel >= recipeLevel - 5)
+                return true;
+            return false;
+        });
+
+        // Then filter on text search
+        $scope.recipeSearch.list = $filter('filter')(recipes, {name: $scope.recipeSearch.text}, function(recipeName, recipeSearch){
+          if (recipeName == undefined || recipeSearch == undefined)
+            return true;
+
+          return recipeName.removeAccent().toUpperCase().indexOf(recipeSearch.removeAccent().toUpperCase()) >= 0;
+      });
       $scope.recipeSearch.selected = Math.min($scope.recipeSearch.selected, $scope.recipeSearch.list.length - 1);
       $scope.recipeSearch.loading = false;
     }, function (err) {
