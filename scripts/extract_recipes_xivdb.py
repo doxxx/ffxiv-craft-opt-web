1f98fade7865fbeba8066525f71269986ceca2b5
Ley21 <52c614a2@opayq.com>
2016-03-11 14:40:15 +0100

    Update extract_recipes_xivdb.py
    Fix and finish xivdb parsing

--- scripts/extract_recipes_xivdb.py
+++ scripts/extract_recipes_xivdb.py
@@ -1,41 +1,46 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 import json
 import itertools
 import operator
 import urllib.request 
+import requests
+import sys
 
-patchNr = "24"
-url = "http://api.xivdb.com/search?one=recipes&where=AND&order=DESC&by=main.id&filters=patch="+patchNr
-response = urllib.request.urlopen(url).read()
-data = json.loads(response.decode('utf-8'))
+url = "http://api.xivdb.com/search?one=recipes"
+r = requests.get(url)
+data = r.json()
 
-
-def makeRecipe(data):
-	
-	r_url = "http://api.xivdb.com/recipe/" + data['id']
-	response = urllib.request.urlopen(r_url).read()
-	data = json.loads(response.decode('utf-8'))
-	
+def makeRecipe(id):
+	id_str = str(id)
+	r_url = "http://api.xivdb.com/recipe/" + id_str
+	r = requests.get(r_url)
+	data = r.json()
+	sys.stdout.flush()
+	sys.stdout.write("\r" + id_str)
 	name =  {
-		'de': data['name_de'],
+	    'de': data['name_de'],
 		'en': data['name_en'],
 		'fr': data['name_fr'],
+		'ja': data['name_ja'],
 	}	
 	r = {
-		'cls': data['classjob']['name'],
-		'name': name,
-		'level': int(data['level_view']),
-		'durability': int(data['durability']),
+		'cls': data['class_name'],
+		'baselevel': int(data['level_view']),
 		'difficulty': int(data['difficulty']),
-		'maxQuality': int(data['quality'])
+		'durability': int(data['durability']),
+		'level': int(data['level']),
+		'maxQuality': int(data['quality']),
+		'name': name,		
+		'stars': int(data['stars']),
 	}
+    
 	return r
-
-recipes = [makeRecipe(jdata)
-           for jdata
-           in data['recipes']['results']]
-print(recipes)
+maxId = data['recipes']['results'][0]['id']
+allRecipeIds = range(1,maxId)
+recipes = [makeRecipe(id)
+           for id
+           in allRecipeIds]
 recipeClassName = operator.itemgetter('cls')
 recipes.sort(key=recipeClassName)
 recipesByClass = {k:list(v) for k,v in itertools.groupby(recipes, recipeClassName)}
@@ -52,3 +57,10 @@ try:
     json.dump(recipesByClass, recipeDbFile)
 finally:
     recipeDbFile.close()
+
+for cls in recipesByClass:
+	file = open(cls+'.json','w')
+	json.dump(recipesByClass[cls],file)
+	file.close()
+	
+	
