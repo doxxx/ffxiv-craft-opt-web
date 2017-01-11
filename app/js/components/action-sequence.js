ceea5e32212bb4f139660df28b01dfa6a51b1435
Gordon Tyler <gordon@doxxx.net>
2017-01-11 17:33:21 -0500

    Refactor action-sequence directive according to Angular Styleguide.

--- app/js/components/action-sequence.js
+++ app/js/components/action-sequence.js
@@ -1,7 +1,11 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('ffxivCraftOptWeb.components')
-  .directive('actionSequence', function () {
+  angular
+    .module('ffxivCraftOptWeb.components')
+    .directive('actionSequence', factory);
+
+  function factory() {
     return {
       restrict: 'E',
       templateUrl: 'components/action-sequence.html',
@@ -14,59 +18,63 @@ angular.module('ffxivCraftOptWeb.components')
         draggable: '=',
         tooltipPlacement: '@'
       },
-      controller: function ($scope, $rootScope, $translate, _allActions, _allClasses, _actionGroups, _actionsByName, _xivdbtooltips, _getActionImagePath) {
-        $scope.actionGroups = _actionGroups;
-
-        $scope.getActionImagePath = _getActionImagePath;
-
-        $scope.actionTooltips = {};
-        $scope.updateActionTooltips = function() {
-          var newTooltips = {};
-          angular.forEach(_actionsByName, function(actionInfo) {
-            var key;
-            if (actionInfo.cls != 'All') {
-              key = actionInfo.cls + actionInfo.shortName;
-            }
-            else {
-              key = $scope.cls + actionInfo.shortName;
-            }
-            newTooltips[actionInfo.shortName] = _xivdbtooltips.actionTooltips[key];
-          });
-          $scope.actionTooltips = newTooltips;
-        };
-        $scope.updateActionTooltips();
-        $scope.$on("tooltipCacheUpdated", function (event) {
-          $scope.updateActionTooltips();
-        });
-        $scope.$watch("cls", function (event) {
-          $scope.updateActionTooltips();
-        });
-
-        $scope._actionClasses = function (action, cls, index) {
-          var classes = {};
-          if ($scope.actionClasses) {
-            classes = $scope.actionClasses(action, cls, index);
-          }
-          return classes;
-        };
-
-        $scope.actionForName = function (name) {
-          return _actionsByName[name];
-        };
-
-        $scope.isActionCrossClass = function (action, cls) {
-          if (!angular.isDefined(action)) {
-            console.error('undefined actionName');
-            return undefined;
-          }
-          var info = _actionsByName[action];
-          if (!angular.isDefined(info)) {
-            console.error('unknown action: %s', action);
-            return undefined;
-          }
-          return info.cls != 'All' &&
-                 info.cls != cls;
-        };
-      }
+      controller: controller
     }
-  });
+  }
+
+  function controller($scope, _actionsByName, _xivdbtooltips, _getActionImagePath) {
+    $scope.getActionImagePath = _getActionImagePath;
+    $scope.cssClassesForAction = cssClassesForAction;
+    $scope.actionForName = actionForName;
+    $scope.isActionCrossClass = isActionCrossClass;
+
+    $scope.actionTooltips = {};
+
+    $scope.$on("tooltipCacheUpdated", updateActionTooltips);
+    $scope.$watch("cls", updateActionTooltips);
+
+    updateActionTooltips();
+
+    //////////////////////////////////////////////////////////////////////////
+
+    function updateActionTooltips() {
+      var newTooltips = {};
+      angular.forEach(_actionsByName, function (actionInfo) {
+        var key;
+        if (actionInfo.cls != 'All') {
+          key = actionInfo.cls + actionInfo.shortName;
+        }
+        else {
+          key = $scope.cls + actionInfo.shortName;
+        }
+        newTooltips[actionInfo.shortName] = _xivdbtooltips.actionTooltips[key];
+      });
+      $scope.actionTooltips = newTooltips;
+    }
+
+    function cssClassesForAction(action, index) {
+      var classes = {};
+      if ($scope.actionClasses) {
+        classes = $scope.actionClasses(action, $scope.cls, index);
+      }
+      return classes;
+    }
+
+    function actionForName(name) {
+      return _actionsByName[name];
+    }
+
+    function isActionCrossClass(action) {
+      if (!angular.isDefined(action)) {
+        console.error('undefined actionName');
+        return undefined;
+      }
+      var info = _actionsByName[action];
+      if (!angular.isDefined(info)) {
+        console.error('unknown action: %s', action);
+        return undefined;
+      }
+      return info.cls != 'All' && info.cls != $scope.cls;
+    }
+  }
+})();
