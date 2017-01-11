bb5b3c2c85abf6d4c466f28548881f1aa9372454
Gordon Tyler <gordon@doxxx.net>
2017-01-11 18:32:00 -0500

    Refactor app.js according to Angular Styleguide.

--- app/js/app.js
+++ app/js/app.js
@@ -1,62 +1,64 @@
-'use strict';
+(function () {
+  'use strict';
 
-// Declare app level module which depends on filters, and services
-angular.module('ffxivCraftOptWeb', [
-  'ui.bootstrap',
-  'ui.router',
-  'pascalprecht.translate',
-  'lvl.directives.dragdrop',
-  'ffxivCraftOptWeb.services',
-  'ffxivCraftOptWeb.services.actions',
-  'ffxivCraftOptWeb.services.localprofile',
-  'ffxivCraftOptWeb.services.recipelibrary',
-  'ffxivCraftOptWeb.services.simulator',
-  'ffxivCraftOptWeb.services.solver',
-  'ffxivCraftOptWeb.services.translateLocalStorage',
-  'ffxivCraftOptWeb.services.xivdbtooltips',
-  'ffxivCraftOptWeb.services.xivdb',
-  'ffxivCraftOptWeb.directives',
-  'ffxivCraftOptWeb.filters',
-  'ffxivCraftOptWeb.controllers',
-  'ffxivCraftOptWeb.components'
-], function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/simulator');
-  $stateProvider
-    .state('crafter-attributes', {
-      url: '/crafter-attributes',
-      templateUrl: 'views/crafter-attributes.html',
-      controller: 'CrafterStatsController'
-    })
-    .state('simulator', {
-      url: '/simulator',
-      templateUrl: 'views/simulator.html',
-      controller: 'SimulatorController'
-    })
-    .state('solver', {
-      url: '/solver',
-      templateUrl: 'views/solver.html',
-      controller: 'SolverController',
-      params: {
-        autoStart: null
-      }
-    })
-    .state('instructions', {
-      url: '/instructions',
-      templateUrl: 'views/instructions.html'
-    })
-    .state('about', {
-      url: '/about',
-      templateUrl: 'views/about.html'
-    })
-})
-  .config(function($translateProvider) {
+  angular
+    .module('ffxivCraftOptWeb', [
+      'ui.bootstrap',
+      'ui.router',
+      'pascalprecht.translate',
+      'lvl.directives.dragdrop',
+      'ffxivCraftOptWeb.services',
+      'ffxivCraftOptWeb.services.actions',
+      'ffxivCraftOptWeb.services.localprofile',
+      'ffxivCraftOptWeb.services.recipelibrary',
+      'ffxivCraftOptWeb.services.simulator',
+      'ffxivCraftOptWeb.services.solver',
+      'ffxivCraftOptWeb.services.translateLocalStorage',
+      'ffxivCraftOptWeb.services.xivdbtooltips',
+      'ffxivCraftOptWeb.services.xivdb',
+      'ffxivCraftOptWeb.directives',
+      'ffxivCraftOptWeb.filters',
+      'ffxivCraftOptWeb.controllers',
+      'ffxivCraftOptWeb.components'
+    ], config);
+
+  function config($stateProvider, $urlRouterProvider, $translateProvider) {
+    $urlRouterProvider.otherwise('/simulator');
+
+    $stateProvider
+      .state('crafter-attributes', {
+        url: '/crafter-attributes',
+        templateUrl: 'views/crafter-attributes.html',
+        controller: 'CrafterStatsController'
+      })
+      .state('simulator', {
+        url: '/simulator',
+        templateUrl: 'views/simulator.html',
+        controller: 'SimulatorController'
+      })
+      .state('solver', {
+        url: '/solver',
+        templateUrl: 'views/solver.html',
+        controller: 'SolverController',
+        params: {
+          autoStart: null
+        }
+      })
+      .state('instructions', {
+        url: '/instructions',
+        templateUrl: 'views/instructions.html'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html'
+      });
+
     // Define the English translations here so that they are immediately
     // available without requiring a network round-trip.
     // Translation keys should be the actual English so that they can be used
     // as-is without having to redefine it here. The exception is when
     // interpolation is required: then the key should be an all uppercase token.
-    $translateProvider.translations('en', {
-    });
+    $translateProvider.translations('en', {});
 
     $translateProvider.useStaticFilesLoader({
       prefix: 'locale/',
@@ -75,4 +77,5 @@ angular.module('ffxivCraftOptWeb', [
     }
 
     $translateProvider.useStorage('_translateLocalStorage');
-  });
+  }
+})();
