dcc8088b48a039d8401d272043c63494ffb9845e
Gordon Tyler <gordon@doxxx.net>
2017-01-11 18:05:12 -0500

    Refactor CharImportController according to Angular Styleguide.

--- app/js/controllers/charimport.js
+++ app/js/controllers/charimport.js
@@ -1,84 +1,95 @@
-"use strict";
+(function () {
+  'use strict';
 
-angular.module('ffxivCraftOptWeb.controllers').controller('CharImportController', function ($scope, $modalInstance, $timeout, _xivdb) {
-  $scope.servers = _xivdb.getServers();
-  $scope.searchVars = {
-    server: $scope.servers[0],
-    name: ""
-  };
-  $scope.searchErrors = [];
-  $scope.chars = {};
+  angular
+    .module('ffxivCraftOptWeb.controllers')
+    .controller('CharImportController', controller);
 
-  $scope.search = function () {
-    $scope.searching = true;
-    delete $scope.results;
-    delete $scope.selected;
+  function controller($scope, $modalInstance, _xivdb) {
+    $scope.search = search;
+    $scope.selectResult = selectResult;
+    $scope.dismissSearchError = dismissSearchError;
+    $scope.importCharacter = importCharacter;
+    $scope.cancel = cancel;
 
-    _xivdb.search($scope.searchVars.name, $scope.searchVars.server).then(function (results) {
-      console.log(results);
-
-      $scope.results = results;
-
-      for (var i = 0; i < $scope.results.length; i++) {
-        var result = $scope.results[i];
-        loadCharacter(result.id);
-      }
-    }, function (err) {
-      $scope.results = null;
-      $scope.searchErrors.push(err);
-    }).finally(function () {
-      $scope.searching = false;
-    });
-  };
-
-  $scope.selectResult = function (id) {
-    $scope.selected = id;
-    $scope.selectedChar = $scope.chars[id];
-  };
-
-  $scope.dismissSearchError = function (index) {
-    $scope.searchErrors.splice(index, 1);
-  };
-
-  $scope.dismissCharError = function (id) {
-    delete $scope.chars[id].error;
-  };
-
-  $scope.import = function () {
-    cancelLoaders();
-    $modalInstance.close($scope.selectedChar);
-  };
-
-  $scope.cancel = function () {
-    cancelLoaders();
-    $modalInstance.dismiss('cancel');
-  };
-
-  function loadCharacter(id) {
-    var promise = _xivdb.getCharacter(id).then(function (char) {
-      console.log("Found character data:", char);
-      if (Object.keys(char.classes).length == 0) {
-        char.error = "No data"
-      }
-      $scope.chars[id] = char;
-    }, function (err) {
-      console.log("No data available for character ID ", id);
-      delete $scope.chars[id].loading;
-      $scope.chars[id].error = err;
-    });
-
-    $scope.chars[id] = {
-      loading: promise
+    $scope.servers = _xivdb.getServers();
+    $scope.searchVars = {
+      server: $scope.servers[0],
+      name: ""
     };
-  }
+    $scope.searchErrors = [];
+    $scope.chars = {};
 
-  function cancelLoaders() {
-    for (var char in $scope.chars) {
-      if ($scope.chars.hasOwnProperty(char)) {
-        if (char.loading) {
-          char.loading.cancel();
+    //////////////////////////////////////////////////////////////////////////
+
+    function search() {
+      $scope.searching = true;
+      delete $scope.results;
+      delete $scope.selected;
+
+      _xivdb.search($scope.searchVars.name, $scope.searchVars.server).then(function (results) {
+        console.log(results);
+
+        $scope.results = results;
+
+        for (var i = 0; i < $scope.results.length; i++) {
+          var result = $scope.results[i];
+          loadCharacter(result.id);
+        }
+      }, function (err) {
+        $scope.results = null;
+        $scope.searchErrors.push(err);
+      }).finally(function () {
+        $scope.searching = false;
+      });
+    }
+
+    function selectResult(id) {
+      $scope.selected = id;
+      $scope.selectedChar = $scope.chars[id];
+    }
+
+    function dismissSearchError(index) {
+      $scope.searchErrors.splice(index, 1);
+    }
+
+    function importCharacter() {
+      cancelLoaders();
+      $modalInstance.close($scope.selectedChar);
+    }
+
+    function cancel() {
+      cancelLoaders();
+      $modalInstance.dismiss('cancel');
+    }
+
+    function loadCharacter(id) {
+      var promise = _xivdb.getCharacter(id).then(function (char) {
+        console.log("Found character data:", char);
+        if (angular.equals(char.classes, {})) {
+          char.error = "No data"
+        }
+        $scope.chars[id] = char;
+      }, function (err) {
+        console.log("No data available for character ID ", id);
+        delete $scope.chars[id].loading;
+        $scope.chars[id].error = err;
+      });
+
+      $scope.chars[id] = {
+        loading: promise
+      };
+    }
+
+    function cancelLoaders() {
+      for (var char in $scope.chars) {
+        if ($scope.chars.hasOwnProperty(char)) {
+          if (char.loading) {
+            char.loading.cancel();
+          }
         }
       }
     }
   }
-});
+
+})();
