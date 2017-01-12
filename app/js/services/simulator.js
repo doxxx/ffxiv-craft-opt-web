86c5683f14357f0bc4289dbb4f4b01857c829bd4
Gordon Tyler <gordon@doxxx.net>
2017-01-12 17:13:06 -0500

    Refactor SimulatorService according to Angular Styleguide.

--- app/js/services/simulator.js
+++ app/js/services/simulator.js
@@ -1,92 +1,95 @@
-'use strict';
+(function () {
+  'use strict';
 
-var SimulationService = function($timeout) {
-  this.$timeout = $timeout;
+  angular
+    .module('ffxivCraftOptWeb.services.simulator', [])
+    .service('_simulator', SimulatorService);
 
-  var worker = new Worker('js/simulationworker.js');
+  function SimulatorService($timeout) {
+    this.$timeout = $timeout;
 
-  var self = this;
-  worker.onmessage = function(e) {
-    if (e.data.success) {
-      self.$timeout(function() {
-        self.callbacks[e.data.id].success(e.data.success);
-      });
-    }
-    else if (e.data.error) {
-      self.$timeout(function() {
-        self.callbacks[e.data.id].error(e.data.error);
-      });
-    }
-    else {
-      console.error('unexpected message from simulation worker: %O', e.data);
-      if (e.data && e.data.id) {
-        self.$timeout(function() {
-          self.callbacks[e.data.id].error({log: '', error: 'unexpected message from simulation worker: ' + e.data});
+    var worker = new Worker('js/simulationworker.js');
+
+    var self = this;
+    worker.onmessage = function (e) {
+      if (e.data.success) {
+        self.$timeout(function () {
+          self.callbacks[e.data.id].success(e.data.success);
         });
       }
+      else if (e.data.error) {
+        self.$timeout(function () {
+          self.callbacks[e.data.id].error(e.data.error);
+        });
+      }
+      else {
+        console.error('unexpected message from simulation worker: %O', e.data);
+        if (e.data && e.data.id) {
+          self.$timeout(function () {
+            self.callbacks[e.data.id].error({log: '', error: 'unexpected message from simulation worker: ' + e.data});
+          });
+        }
+      }
+    };
+
+    this.worker = worker;
+
+    this.currentId = 0;
+    this.callbacks = {};
+  }
+
+  SimulatorService.$inject = ['$timeout'];
+
+  SimulatorService.prototype.runMonteCarloSim = function(settings, success, error) {
+    if (settings.sequence.length <= 0) {
+      error({log: '', error: 'empty sequence'});
+      return;
     }
+    if (settings.recipe.startQuality === undefined) {
+      settings.recipe = angular.copy(settings.recipe);
+      settings.recipe.startQuality = 0;
+    }
+
+    var id = this.nextId();
+
+    this.callbacks[id] = {
+      success: success,
+      error: error
+    };
+
+    this.worker.postMessage({
+      id: id,
+      type: 'montecarlo',
+      settings: settings
+    });
   };
 
-  this.worker = worker;
+  SimulatorService.prototype.runProbabilisticSim = function (settings, success, error) {
+    if (settings.sequence.length <= 0) {
+      error({log: '', error: 'empty sequence'});
+      return;
+    }
+    if (settings.recipe.startQuality === undefined) {
+      settings.recipe = angular.copy(settings.recipe);
+      settings.recipe.startQuality = 0;
+    }
 
-  this.currentId = 0;
-  this.callbacks = {};
-};
+    var id = this.nextId();
 
-SimulationService.$inject = ['$timeout'];
+    this.callbacks[id] = {
+      success: success,
+      error: error
+    };
 
-SimulationService.prototype.runMonteCarloSim = function(settings, success, error) {
-  if (settings.sequence.length <= 0) {
-    error({log: '', error: 'empty sequence'});
-    return;
-  }
-  if (settings.recipe.startQuality === undefined) {
-    settings.recipe = angular.copy(settings.recipe);
-    settings.recipe.startQuality = 0;
-  }
-
-  var id = this.nextId();
-
-  this.callbacks[id] = {
-    success: success,
-    error: error
+    this.worker.postMessage({
+      id: id,
+      type: 'prob',
+      settings: settings
+    });
   };
 
-  this.worker.postMessage({
-    id: id,
-    type: 'montecarlo',
-    settings: settings
-  });
-};
-
-SimulationService.prototype.runProbabilisticSim = function (settings, success, error) {
-  if (settings.sequence.length <= 0) {
-    error({log: '', error: 'empty sequence'});
-    return;
-  }
-  if (settings.recipe.startQuality === undefined) {
-    settings.recipe = angular.copy(settings.recipe);
-    settings.recipe.startQuality = 0;
-  }
-
-  var id = this.nextId();
-
-  this.callbacks[id] = {
-    success: success,
-    error: error
+  SimulatorService.prototype.nextId = function () {
+    this.currentId += 1;
+    return this.currentId;
   };
-
-  this.worker.postMessage({
-    id: id,
-    type: 'prob',
-    settings: settings
-  });
-};
-
-SimulationService.prototype.nextId = function () {
-  this.currentId += 1;
-  return this.currentId;
-};
-
-angular.module('ffxivCraftOptWeb.services.simulator', []).
-  service('_simulator', SimulationService);
+})();
