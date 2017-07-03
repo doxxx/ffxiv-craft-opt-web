4d9b1711a8b8a1cbab0c4c40002a70fa107ae676
Gordon Tyler <gordon@doxxx.net>
2017-07-03 12:07:12 -0400

    Fix and rename recipedb normalization script.

--- app/data/recipedb/normalize.py
+++ app/data/recipedb/normalize.py
@@ -1,6 +1,6 @@
 import json
 
-for n in ["Armorer","Blacksmith","Carpenter","Culinarian","Goldsmith","Leatherworker","Weaver"]:
+for n in ["Alchemist","Armorer","Blacksmith","Carpenter","Culinarian","Goldsmith","Leatherworker","Weaver"]:
     with open(f"{n}.json", mode="rt", encoding="utf-8") as f:
         recipes = json.load(f)
     with open(f"{n}.json", mode="wt", encoding="utf-8") as f:
