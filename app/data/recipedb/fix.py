d87b1b8f45c1d888a61b8dc6a35407f457a9e2b2
Gordon Tyler <gordon@doxxx.net>
2017-07-03 12:01:42 -0400

    Fix language ordering in recipe names.

---
+++ app/data/recipedb/fix.py
@@ -0,0 +1,7 @@
+import json
+
+for n in ["Armorer","Blacksmith","Carpenter","Culinarian","Goldsmith","Leatherworker","Weaver"]:
+    with open(f"{n}.json", mode="rt", encoding="utf-8") as f:
+        recipes = json.load(f)
+    with open(f"{n}.json", mode="wt", encoding="utf-8") as f:
+        json.dump(recipes, f, indent=2, sort_keys=True, ensure_ascii=False)
