13552d492a12b8bcafd4bb4ff079d130e660e797
Gordon Tyler <gordon@doxxx.net>
2017-07-16 09:30:03 -0400

    Generate macro for setting up cross-class actions.

--- app/js/components/macros.js
+++ app/js/components/macros.js
@@ -13,17 +13,19 @@
       templateUrl: 'components/macros.html',
       scope: {
         sequence: '=',
+        cls: '=',
         options: '='
       },
       controller: controller
     }
   }
 
-  function controller($scope, $translate, _actionsByName, _allActions) {
+  function controller($scope, $translate, _actionsByName, _allActions, _isActionCrossClass) {
     $scope.macroList = [];
 
     $scope.$on('$translateChangeSuccess', update);
     $scope.$watchCollection('sequence', update);
+    $scope.$watch('cls', update);
     $scope.$watchCollection('options', update);
 
     update();
@@ -35,7 +37,18 @@
         return;
       }
 
-      $scope.macroList = buildMacroList(buildMacroLines(extractBuffs()));
+      var macroList = [];
+
+      if ($scope.options.setupCrossClassActions) {
+        var crossClassActionSetupLines = buildCrossClassActionSetupLines($scope.options, $scope.sequence, $scope.cls);
+        macroList.push(crossClassActionSetupLines.join(''))
+      }
+
+      var sequenceLines = buildSequenceLines($scope.options, $scope.sequence, extractBuffs());
+      var sequenceMacroList = buildMacroList($scope.options, sequenceLines);
+      macroList = macroList.concat(sequenceMacroList);
+
+      $scope.macroList = macroList;
     }
 
     function extractBuffs() {
@@ -49,14 +62,51 @@
       return buffs;
     }
 
-    function buildMacroLines(buffs) {
-      var waitString = '<wait.' + $scope.options.waitTime + '>';
-      var buffWaitString = '<wait.' + $scope.options.buffWaitTime + '>';
+    function soundEffect(num) {
+      return '<se.' + num + '>';
+    }
+
+    function buildCrossClassActionSetupLines(options, sequence, cls) {
+      var crossClass = {};
+      for (var i = 0; i < sequence.length; i++) {
+        var action = sequence[i];
+        if (_isActionCrossClass(action, cls) && !crossClass[action]) {
+          crossClass[action] = true;
+        }
+      }
+      crossClass = Object.keys(crossClass);
+
+      var lines = [];
+      if (crossClass.length > 0) {
+        lines.push('/aaction clear\n');
+      }
+
+      for (var i = 0; i < crossClass.length; i++) {
+        var action = crossClass[i];
+        var info = _actionsByName[action];
+        if (info) {
+          var actionName = $translate.instant(info.name);
+          var line = '/aaction "' + actionName + '" on\n';
+          lines.push(line);
+        }
+        else {
+          lines.push('/echo Error: Unknown action ' + action);
+        }
+      }
+
+      lines.push('/echo Cross-class action setup complete ' + soundEffect(options.stepSoundEffect) + '\n');
+
+      return lines;
+    }
+
+    function buildSequenceLines(options, sequence, buffs) {
+      var waitString = '<wait.' + options.waitTime + '>';
+      var buffWaitString = '<wait.' + options.buffWaitTime + '>';
 
       var lines = [];
 
-      for (var i = 0; i < $scope.sequence.length; i++) {
-        var action = $scope.sequence[i];
+      for (var i = 0; i < sequence.length; i++) {
+        var action = sequence[i];
         var info = _actionsByName[action];
         if (info) {
           var actionName = $translate.instant(info.name);
@@ -74,13 +124,11 @@
           lines.push('/echo Error: Unknown action ' + action);
         }
       }
+
       return lines;
     }
 
-    function buildMacroList(lines) {
-      var stepSoundEffect = '<se.' + $scope.options.stepSoundEffect + '>';
-      var finishSoundEffect = '<se.' + $scope.options.finishSoundEffect + '>';
-
+    function buildMacroList(options, lines) {
       var macroList = [];
 
       var macroString = '';
@@ -93,7 +141,7 @@
 
         if (macroLineCount == MAX_LINES-1) {
           if (lines.length - (j + 1) > 1) {
-            macroString += '/echo Macro #' + macroIndex + ' complete ' + stepSoundEffect + '\n';
+            macroString += '/echo Macro #' + macroIndex + ' complete ' + soundEffect(options.stepSoundEffect) + '\n';
             macroList.push(macroString);
             macroString = '';
             macroLineCount = 0;
@@ -104,7 +152,7 @@
 
       if (macroLineCount > 0) {
         if (macroLineCount < MAX_LINES) {
-          macroString += '/echo Macro #' + macroIndex + ' complete ' + finishSoundEffect + '\n';
+          macroString += '/echo Macro #' + macroIndex + ' complete ' + soundEffect(options.finishSoundEffect) + '\n';
         }
         macroList.push(macroString)
       }
