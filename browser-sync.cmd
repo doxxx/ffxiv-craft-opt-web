1afb9cbaf23ab6746c023b658fb6899934e0eed7
Gordon Tyler <gordon@doxxx.net>
2017-07-23 16:11:27 -0400

    Simplify and rename browser-sync script.

--- browser-sync.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd app
-browser-sync start --server --port 8001 --files "css/*,js/*,js/*/*,partials/*,index.html"
---
+++ start-browser-sync.cmd
@@ -0,0 +1,2 @@
+@echo off
+browser-sync start --port 8001 --server app --files app
