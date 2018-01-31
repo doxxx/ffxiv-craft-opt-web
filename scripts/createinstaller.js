ba56216b52ff35150c476e82d3c42e5db7f34859
dennis.kirschstein <dennis.kirschstein@cstx.de>
2018-01-31 12:42:44 +0100

    Added scripts and build script for windows installer

---
+++ scripts/createinstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'release-builds')
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'ffxiv-craft-opt-win32-ia32/'),
+    authors: 'Gordon Tyler',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'ffxiv-craft-opt-app.exe',
+    setupExe: 'FFXIVCraftingOpt.exe',
+    setupIcon: path.join(rootPath, 'icons', 'icon.ico')
+  })
+}
