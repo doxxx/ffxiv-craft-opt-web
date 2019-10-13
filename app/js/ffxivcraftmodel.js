edc87122bd17b1be1957ff9a42896eb6e67bcf1e
X-bear <522168288@qq.com>
2019-10-13 23:45:25 +0800

    add control limit 5.0

--- app/js/ffxivcraftmodel.js
+++ app/js/ffxivcraftmodel.js
@@ -249,6 +249,7 @@ function ApplyModifiers(s, action, condition) {
     //=================
     var craftsmanship = s.synth.crafter.craftsmanship;
     var control = s.synth.crafter.control;
+    var originalControl = s.synth.crafter.control
 
     // Effects modifying control
     if (AllActions.innerQuiet.shortName in s.effects.countUps) {
@@ -258,6 +259,8 @@ function ApplyModifiers(s, action, condition) {
     if (AllActions.innovation.shortName in s.effects.countDowns) {
         control += 0.5 * s.synth.crafter.control;
     }
+    // add control limit 5.0
+    control - originalControl >= 3000 ? (control = 3000 + originalControl) : 0
 
     // Effects modifying level difference
     var effCrafterLevel = getEffectiveCrafterLevel(s.synth);
